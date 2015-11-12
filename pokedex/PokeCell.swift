4bf548100b4a43ba0a7c0f0327bee275166bc214
mikepitre <michaelepitre@gmail.com>
2015-11-12 10:47:17 -0600

    created PokeCell class, connected outlets

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Mike Pitre on 11/12/15.
+//  Copyright © 2015 Mike Pitre. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
