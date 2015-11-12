3f08783e3d5181d106a721baee59bcfaff70235c
mikepitre <michaelepitre@gmail.com>
2015-11-12 13:54:49 -0600

    temporarily hardcoded in # of cells to display with images

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
+    
+    @IBOutlet weak var collection: UICollectionView!
 
     override func viewDidLoad()
         {
         super.viewDidLoad()
+        collection.delegate = self
+        collection.dataSource = self
         }
 
     override func didReceiveMemoryWarning()
@@ -23,24 +27,26 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
         {
-            if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell
-                {
-                return cell
-                }
-            else
-                {
-                return PokeCell()
-                }
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell
+            {
+            let pokemon = Pokemon(name: "Test", pokedexId: indexPath.row + 1)
+            cell.configureCell(pokemon)
+            return cell
+            }
+        else
+            {
+            return UICollectionViewCell()
+            }
         }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath)
         {
-        <#code#>
+            
         }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
         {
-        return 30
+        return 718
         }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
@@ -50,7 +56,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize
         {
-        CGSizeMake(105, 105)
+        return CGSizeMake(105, 105)
         }
 
 
