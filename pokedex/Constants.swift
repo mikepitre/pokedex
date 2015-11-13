67d888370d4417a46b17b0cf51f58b773c322665
mikepitre <michaelepitre@gmail.com>
2015-11-13 15:14:28 -0600

    fixed bug, typo in constant api url

--- pokedex/Constants.swift
+++ pokedex/Constants.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 let URL_BASE = "http://pokeapi.co"
-let URL_POKEMON = "/api/v1/pokedex/"
+let URL_POKEMON = "/api/v1/pokemon/"
 
 typealias DownloadComplete = () -> ()
--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -20,7 +20,7 @@ class Pokemon
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionTxt: String!
-    private var _pokemonURL: String!
+    private var _pokemonUrl: String!
     
     var name: String
         {
@@ -37,12 +37,15 @@ class Pokemon
         self._name = name
         self._pokedexId = pokedexId
             
-        _pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(self._pokedexId)/"
+        _pokemonUrl = "\(URL_BASE)\(URL_POKEMON)\(self._pokedexId)/"
         }
     
     func downloadPokemonDetails(completed: DownloadComplete)
         {
-        
+        let url = NSURL(string: _pokemonUrl)!
+        Alamofire.request(.GET, url).responseJSON { response in
+            let result = response.result
+            print(result.value.debugDescription)
+            }
         }
-    
 }
